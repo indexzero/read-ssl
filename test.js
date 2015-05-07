7d9972be169bcbdf04dff280f1cf6e0f71cdf668
indexzero <charlie.robbins@gmail.com>
2015-05-07 16:48:30 -0700

    [test] Added tests.

---
+++ test.js
@@ -0,0 +1,50 @@
+/*
+ * test.js: Make sure reading with various options works
+ *
+ * (C) 2015, Charlie Robbins.
+ *
+ */
+
+var path = require('path'),
+    http = require('http'),
+    test = require('tape'),
+    readSsl = require('./');
+
+test('key, cert, ca (single)', function (t) {
+  t.plan(5);
+
+  readSsl({
+    root: __dirname,
+    key: 'index.js',
+    cert: 'LICENSE',
+    ca: 'README.md'
+  }, function (err, files) {
+    console.dir(err);
+    t.error(err);
+    t.equals(typeof files, 'object');
+    t.ok(Buffer.isBuffer(files.key), 'has a Buffer `key`');
+    t.ok(Buffer.isBuffer(files.cert), 'has a Buffer `cert`');
+    t.ok(Buffer.isBuffer(files.ca), 'has a Buffer `ca`');
+  });
+});
+
+test('key, cert, ca (array)', function (t) {
+  t.plan(7);
+
+  readSsl({
+    root: __dirname,
+    key: 'index.js',
+    cert: 'LICENSE',
+    ca: ['README.md', '.gitignore']
+  }, function (err, files) {
+    console.dir(err);
+    t.error(err);
+    t.equals(typeof files, 'object');
+    t.ok(Buffer.isBuffer(files.key), 'has a Buffer `key`');
+    t.ok(Buffer.isBuffer(files.cert), 'has a Buffer `cert`');
+    t.ok(Array.isArray(files.ca), 'has an Array `ca`');
+    files.ca.forEach(function (ca, i) {
+      t.ok(Buffer.isBuffer(ca), 'has a Buffer `ca` ' + i);
+    });
+  });
+});
